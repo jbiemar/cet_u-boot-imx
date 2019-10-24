7649581de50d7b04427f9d39bcef26066f526721
jbiemar <jb@altaneos.com>
2019-10-24 15:10:51 +0200

    test splash

--- board/freescale/imx6qpfatmaninviewx/imx6qpfatmaninviewx.c
+++ board/freescale/imx6qpfatmaninviewx/imx6qpfatmaninviewx.c
@@ -805,8 +805,8 @@ int setup_splash(void)
 
 	snprintf(cmdarg, sizeof(cmdarg), "load mmc 1:2 %s /usr/share/splash_bmp/splash-big.bmp", strldaddr);
 	printf("Command : %s", cmdarg);
-	run_command(cmdarg, 0);
-	ret = bmp_display(ldaddr, 0, 0);
+	/*run_command(cmdarg, 0);
+	ret = bmp_display(ldaddr, 0, 0);*/
 
 	return ret;
 }

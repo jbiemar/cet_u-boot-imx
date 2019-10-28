cedfb3a18b72dd721c47b3d9280ee8aed4803eee
jbiemar <jb@altaneos.com>
2019-10-28 11:10:34 +0100

    change video mode u boot

--- include/configs/imx6qpfatmaninviewx_common.h
+++ include/configs/imx6qpfatmaninviewx_common.h
@@ -145,7 +145,7 @@
 		"fi\0" \
 	EMMC_ENV	  \
 	"smp=" CONFIG_SYS_NOSMP "\0"\
-	CONFIG_LCD_ENV_SETTINGS \
+	"mmcargs=setenv bootargs console=${console},${baudrate} root=${mmcroot}\0" \
 	"loadbootscript=" \
 		"fatload mmc ${mmcdev}:${mmcpart} ${loadaddr} ${script};\0" \
 	"bootscript=echo Running bootscript from mmc ...; " \

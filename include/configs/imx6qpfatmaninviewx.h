b562ec894c79b94df6eb1ade03554ccd3d112d73
jbiemar <jb@altaneos.com>
2019-10-31 17:13:14 +0100

    clean of uboot ok

--- include/configs/imx6qpfatmaninviewx.h
+++ include/configs/imx6qpfatmaninviewx.h
@@ -281,12 +281,4 @@
 #define CONFIG_G_DNL_PRODUCT_NUM	0xa4a5
 #define CONFIG_G_DNL_MANUFACTURER	"FSL"
 
-/* USB Device Firmware Update support */
-/*#define CONFIG_CMD_DFU
-#define CONFIG_USB_FUNCTION_DFU
-#define CONFIG_DFU_MMC
-#if defined(CONFIG_SYS_USE_SPINOR)
-#define CONFIG_DFU_SF
-#endif*/
-
 #endif                         /* __IMX6QPFATMANINVIEWX_CONFIG_H */
